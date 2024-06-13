8a799d579f1f937b38e20c84ed7246e40cde927b
Eduardo Sanchez <80008158+lorf543@users.noreply.github.com>
2024-06-12 22:48:52 -0400

    allow host

--- carnet_m/settings.py
+++ carnet_m/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=29%1d8%6@)kv-a9=aen4qhs)w6s(%fkj#5edtjm5h$+upshma
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','vigilant-renewal-production.up.railway.app']
 
 
 # Application definition
