12d18f5af4fb9051adbd7c757e702cb67e153883
Eduardo Sanchez <80008158+lorf543@users.noreply.github.com>
2024-06-13 23:40:50 -0400

    DB fix

--- carnet_m/settings.py
+++ carnet_m/settings.py
@@ -130,26 +130,26 @@ WSGI_APPLICATION = 'carnet_m.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'railway',
-#         'USER': 'postgres',
-#         'PASSWORD': 'ntVuiFfaUuEpzAcqMaiUJaRXklaXsArY',
-#         'HOST': 'roundhouse.proxy.rlwy.net',
-#         'PORT': '27747',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'ntVuiFfaUuEpzAcqMaiUJaRXklaXsArY',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '27747',
+    }
+}
 
 
 
